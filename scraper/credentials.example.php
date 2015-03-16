a1804ef575d2120c80a4eb94e61d2dfc2e650442
danny.brown <danny@dynamit.com>
2015-03-16 14:10:23 -0400

    Better naming for credentials and adding MySQL Credentials

--- scraper/credentials.example.php
+++ scraper/credentials.example.php
@@ -1,8 +1,17 @@
 <?php
 
-$hostname = '{mail.lug.io:143/notls}INBOX';
-$username = 'abandoned-domains@lug.io';
-$password = 'xxx';
-$ApiKey   = 'xxx';
+// INBOX
+$Email_Host 		= '{mail.lug.io:143/notls}INBOX';
+$Email_User 		= 'xxx';
+$Email_Pass			= 'xxx';
+
+// JSON WHOIS
+$JSONWHOIS_ApiKey	= 'xxx';
+
+// MYSQL
+$MySQL_Host 		= "mysql.vtxt.us";
+$MySQL_DBname		= "xxx";
+$MySQL_User 		= "xxx";
+$MySQL_Pass 		= "xxx"
 
 ?>
